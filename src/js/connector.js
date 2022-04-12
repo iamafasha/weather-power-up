f5384bc658b3bae6c98ae2d7fde344a39ba2feba
Afasha Isakiye <afasha.isakiye@tmcg.co.ug>
2022-04-12 21:44:47 +0300

    Initial

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,14 +1,26 @@
-console.log('Hello World!');
+const moment = require('moment');
+
+function getCardMomentTime(card_id){
+  return moment((new Date(1000*parseInt(card_id.substring(0,8),16)))).fromNow();
+}
 
 window.TrelloPowerUp.initialize({
   'card-badges': function(t, opts) {
     // return an array of card badges for the given card
     return t.card('all')
-    .then(function(card) {
-      console.log(card);
-      return [{
-        text: card.idShort
-      }];
+      .then(function(card) {
+        const card_id = card.id;
+        return [
+          {
+            dynamic: function () {
+              return {
+                text: getCardMomentTime(card_id),
+                icon: "assests/created_at.svg",
+                refresh: 30, // in seconds
+              };
+            },
+          }
+        ];
     })
   }
 });
